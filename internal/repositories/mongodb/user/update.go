1ddd446d958d38262125b94e6d068d6fd4153ad3
Rolando Ramirez <7200ws@gmail.com>
2024-10-25 21:51:24 -0500

    second commit

--- internal/repositories/mongodb/user/update.go
+++ internal/repositories/mongodb/user/update.go
@@ -1,7 +1,30 @@
 package user
 
-import "goportfolio/internal/domain"
+import (
+	"context"
+	"goportfolio/internal/domain"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 func (r *Repository) Update(user domain.User) (string, error) {
 
+	collection := r.dbclient.Database(r.dbname).Collection(r.collname)
+	// idColl := user.ID
+	objID, err := primitive.ObjectIDFromHex(user.ID.Hex())
+	if err != nil {
+		return "", err
+	}
+
+	filter := bson.M{"_id": objID}
+
+	update := bson.M{"$set": user}
+
+	result, err := collection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		return "", err
+	}
+
+	return string(result.ModifiedCount), nil
 }
